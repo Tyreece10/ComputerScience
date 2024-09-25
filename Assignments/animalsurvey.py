417122992847b4dca4144e84d9d0a8477384e474
Tyreece10 <835240@mystma.org>
2024-09-25 09:50:55 -0500

    Converting Data

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,6 +1,6 @@
 favorite_animal = input("What is your favorite animal?:\n ")        #These are the questions asked to the user.
 animal_food = input("What food does the animal eat?:\n ")       #The questions respond to the information of the animal.
-animal_live = input("Where does your animal live?:\n ")
+animal_live = input("Where does your animal live?:\n ")     #What they input is stored under the questions
 animal_color = input("What color is the animal?:\n ")
 animal_fact = input("What's a fact about the animal?:\n ")
 animal_like = input("Why do you like the animal?:\n ")
