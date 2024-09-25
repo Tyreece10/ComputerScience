af93e2ee39202da48f691d7f17b5f7048caa8b53
Tyreece10 <835240@mystma.org>
2024-09-25 08:46:55 -0500

    Animal Survey

---
+++ Unit 2 - Functions and Variables/partice_sept-24.py
@@ -0,0 +1,9 @@
+
+number = int(input("Enter a nummber\n>"))       #Asking for input and declaring a variable
+number = int(number)        #Converting input to an integer, assigning
+print(number * 10)      #printing input times 10
+
+print("Welcome to WORD SMASH!")
+word_one = input("Enter the first word!")       #Ask for 1st word
+word_two = input("Enter the second word!\n>")
+print(word_one + word_two)
