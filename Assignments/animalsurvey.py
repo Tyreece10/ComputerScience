3adf45c5dad26fb6207bc11493b3146c44ece8a5
Tyreece10 <835240@mystma.org>
2024-09-25 09:30:11 -0500

    Animal Survey

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,7 +1,8 @@
-favorite_animal = input("What is your favorite animal?:\n ")
-animal_food = input("What food does the animal eat?:\n ")
+favorite_animal = input("What is your favorite animal?:\n ")        #These are the questions asked to the user.
+animal_food = input("What food does the animal eat?:\n ")       #The questions respond to the information of the animal.
 animal_live = input("Where does your animal live?:\n ")
 animal_color = input("What color is the animal?:\n ")
 animal_fact = input("What's a fact about the animal?:\n ")
 animal_like = input("Why do you like the animal?:\n ")
 print("Your favorite animal is a "+favorite_animal + " and your animal eats "+animal_food + ". Your animal lives in "+animal_live + " and your animal is "+ animal_color + ". A fact about the animal is "+animal_fact + " and you like the animal because "+animal_like +".")
+#The sum of all the variable together using the print function/variable/concatenation.
--- Unit 2 - Functions and Variables/partice_sept-24.py
+++ Unit 2 - Functions and Variables/partice_sept-24.py
@@ -4,6 +4,6 @@ number = int(number)        #Converting input to an integer, assigning
 print(number * 10)      #printing input times 10
 
 print("Welcome to WORD SMASH!")
-word_one = input("Enter the first word!")       #Ask for 1st word
+word_one = input("Enter the first word!\n>")       #Ask for 1st word
 word_two = input("Enter the second word!\n>")
 print(word_one + word_two)
