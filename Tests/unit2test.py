e26703767b4e5d0a9ce33f834c2e8706905e873d
Tyreece10 <835240@mystma.org>
2024-10-03 09:26:40 -0500

    15 point

--- Tests/unit2test.py
+++ Tests/unit2test.py
@@ -1,4 +1,4 @@
-word1 = input("What is your first random word?\n>")     #I'll ask for a word and store it in a variable.
-word2 = input("What is your sencond random word?\n>")
-word3 = input("What is your thrid random word?\n>")
-print(""+word1 + " "+word2 +" "+word3 + ".")        #I would put the variable in a sentence together.
+word_1 = input("What is your first random word?\n>")     #I'll ask for a word and store it in a variable.
+word_2 = input("What is your sencond random word?\n>")
+word_3 = input("What is your thrid random word?\n>")
+print(""+word_1 + " "+word_2 +" "+word_3 + ".")        #I would put the variable in a sentence together.
