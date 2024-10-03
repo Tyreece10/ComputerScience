8738e415a172bb202eb673b893b7921f4e798424
Tyreece10 <835240@mystma.org>
2024-10-03 09:44:27 -0500

    3 point

--- Tests/unit2test3.py
+++ Tests/unit2test3.py
@@ -1,4 +1,7 @@
 def data_three():
-    word1 = input("Give me a word")
-    int1 = input("Give me a integer")
-    float1 = input("Gi")
+    word_1 = input("Give me a word\n>")
+    int_1 = int(input("Give me a integer\n>"))
+    float_1 = float(input("Give me a decimal number\n>"))
+    float(str(print(""+float_1 + int_1 +""+word_1)))
+
+data_three()
