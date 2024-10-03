d3589320f5b5ff0416f87bbc371f7c98ef26df95
Tyreece10 <835240@mystma.org>
2024-10-03 09:15:21 -0500

    7 points

---
+++ Tests/unit2test2.py
@@ -0,0 +1,11 @@
+def add_three(x, y, a):
+    str(print(x + y + a))
+
+int1 = int(input("Give me one integer\n>"))
+int1 = int(int1)
+int2 = int(input("Give me the second integer\n>"))
+int2 = int(int2)
+int3 = int(input("Give me the third integer\n>"))
+int3 = int(int3)
+
+add_three(int1, int2, int3)
