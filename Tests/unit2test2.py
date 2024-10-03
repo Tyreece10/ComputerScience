9cb4b14b9f3bc0447873c1b9b0f14d029f2fee2f
Tyreece10 <835240@mystma.org>
2024-10-03 09:25:59 -0500

    7 point

--- Tests/unit2test2.py
+++ Tests/unit2test2.py
@@ -1,11 +1,11 @@
 def add_three(x, y, a):     #I created a function for adding the future 3 variables
     str(print(x + y + a))
 
-int1 = int(input("Give me one integer\n>"))     #I asked for the first number
-int1 = int(int1)        #converted it to string
-int2 = int(input("Give me the second integer\n>"))      #Asked for second number
-int2 = int(int2)        #converted
-int3 = int(input("Give me the third integer\n>"))           #third
-int3 = int(int3)    #converted
+int_1 = int(input("Give me one integer\n>"))     #I asked for the first number
+int_1 = int(int_1)        #converted it to string
+int_2 = int(input("Give me the second integer\n>"))      #Asked for second number
+int_2 = int(int_2)        #converted
+int_3 = int(input("Give me the third integer\n>"))           #third
+int_3 = int(int_3)    #converted
 
-add_three(int1, int2, int3)     #Then I used he function to add the 3 int together
+add_three(int_1, int_2, int_3)     #Then I used he function to add the 3 int together
--- Tests/unit2test3.py
+++ Tests/unit2test3.py
@@ -1,3 +1,4 @@
 def data_three():
     word1 = input("Give me a word")
-    
+    int1 = input("Give me a integer")
+    float1 = input("Gi")
