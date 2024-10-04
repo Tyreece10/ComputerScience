629286ea02356281bbfe46860a2c14047d4c9b7c
Tyreece10 <835240@mystma.org>
2024-10-04 09:41:13 -0500

    BooleanQuiz

--- Tests/unit2test3.py
+++ Tests/unit2test3.py
@@ -2,6 +2,6 @@ def data_three():
     word_1 = input("Give me a word\n>")
     int_1 = int(input("Give me a integer\n>"))
     float_1 = float(input("Give me a decimal number\n>"))
-    float(str(print(""+float_1 + int_1 +""+word_1)))
+    str(print(""+float_1 + "+int_1 +" + ""+word_1))
 
 data_three()
---
+++ Unit 3 Conditionnals and Exceptions/Bool.py
@@ -0,0 +1,5 @@
+answer = "cube"
+count = 1
+print(answer == "cube" and count < 10)
+pizza = 400
+print(pizza == 200)
