fbc3a895219c6269fe366b6767cec0a7246ad3b4
Tyreece10 <835240@mystma.org>
2024-11-15 09:49:10 -0600

    DICE

---
+++ Assignments/dice.py
@@ -0,0 +1,18 @@
+from adafruit_circuitplayground import cp
+import random
+import time
+
+# Main loop
+while True:
+    if cp.button_a:
+        num_pixels = random.randint(1, 10)
+        
+        # Light up the number of pixels in a chosen color (e.g., blue)
+        for i in range(num_pixels):
+            cp.pixels[i] = (184, 2, 26)
+
+        time.sleep(0.2)
+    
+    if cp.button_b:
+        cp.pixels.fill((0, 0, 0))
+        time.sleep(0.2)
