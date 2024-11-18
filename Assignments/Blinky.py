992c447d53038b91850b60757902625a32f54da8
Tyreece10 <835240@mystma.org>
2024-11-18 09:19:20 -0600

    Woosh

--- Assignments/Blinky.py
+++ Assignments/Blinky.py
@@ -1,13 +1,9 @@
 from adafruit_circuitplayground import cp
 import time
-
-
 while True:
     for i in range(10):
         cp.pixels[i] = (0, 20, 0)
-    time.sleep(0.367)
-
-        
+    time.sleep(0.367) 
     for i in range(10):
         cp.pixels[i] = (0, 0, 0)
     time.sleep(0.367)
